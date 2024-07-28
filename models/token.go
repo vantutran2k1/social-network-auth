d166c8621c92afb0c651ec4a5ca694fc901437b4
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-07-28 22:04:57 +0700

    Update model functions

--- models/token.go
+++ models/token.go
@@ -12,3 +12,28 @@ type Token struct {
 	IssuedAt  time.Time `json:"issued_at" gorm:"not null"`
 	ExpiresAt time.Time `json:"expires_at" gorm:"not null"`
 }
+
+func (token *Token) Save(db *gorm.DB) error {
+	return db.Create(token).Error
+}
+
+func (token *Token) Validate(db *gorm.DB, tokenString string) bool {
+	dbToken := Token{}
+	err := db.Where("token = ?", tokenString).First(&dbToken).Error
+	if err != nil {
+		return false
+	}
+
+	return dbToken.ExpiresAt.After(time.Now())
+}
+
+func (token *Token) Revoke(db *gorm.DB, tokenString string) error {
+	dbToken := Token{}
+	err := db.Where("token = ?", tokenString).First(&dbToken).Error
+	if err != nil {
+		return err
+	}
+
+	dbToken.ExpiresAt = time.Now()
+	return db.Save(&dbToken).Error
+}
--- models/user.go
+++ models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"time"
 )
@@ -8,8 +10,37 @@ import (
 type User struct {
 	gorm.Model
 	Username  string    `json:"username" gorm:"unique, not null"`
-	Password  string    `json:"password" gorm:"not null"`
+	Password  string    `json:"-"`
 	Email     string    `json:"email" gorm:"unique, not null"`
 	CreatedAt time.Time `json:"created_at" gorm:"not null"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"not null"`
+}
+
+func (user *User) Register(db *gorm.DB) error {
+	err := db.Where("username = ?", user.Username).First(&User{}).Error
+	if err == nil {
+		return errors.New("username already exists")
+	}
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return err
+		}
+
+		user.Password = string(hashedPassword)
+		return db.Create(&user).Error
+	}
+
+	return err
+}
+
+func (user *User) Authenticate(db *gorm.DB, password string) bool {
+	err := db.Where("username = ?", user.Username).First(&user).Error
+	if err != nil {
+		return false
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	return err == nil
 }
